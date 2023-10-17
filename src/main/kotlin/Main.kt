fc4635d320a5d24996ab8adc8af2f142028fff21
Dmitry <boetz@yandex.ru>
2023-10-17 23:59:34 +0700

    HomeWork2_1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,35 +1,36 @@
 fun main() {
 
-    val timeAgo = 60*60*24*6.0
+    val timeAgo = 14*60
     val result = "был(а) " + agoToText(timeAgo)
     println(result)
 }
 
-fun agoToText(time: Double): String {
+fun agoToText(time: Int): String {
     val compare: Boolean = true
     return when (compare) {
-        0 < time && time<=60 -> "только что"
-        60 < time && time<= 60 * 60 -> "была в сети " + time/60 + " "+ fixMinutes(time) + " назад"
-        60 * 60 < time && time <= 24*60*60 -> "была в сети " + time/(60*60) + " " +fixHours(time)+ " назад"
-        24*60*60 < time && time <= 2*24*60*60 -> "была в сети вчера"
-        2*24*60*60 < time && time <= 3*24*60*60 -> "была в сети позавчера"
-        else -> "была в сети давно"
+        0 < time && time <= 60 -> "только что"
+        60 < time && time <= 60 * 60 -> "в сети " + time / 60 + " " + fixMinutes(time) + " назад"
+        60 * 60 < time && time <= 24 * 60 * 60 -> "в сети " + time / (60 * 60) + " " + fixHours(time) + " назад"
+        24 * 60 * 60 < time && time <= 2 * 24 * 60 * 60 -> "в сети вчера"
+        2 * 24 * 60 * 60 < time && time <= 3 * 24 * 60 * 60 -> "в сети позавчера"
+        else -> "в сети давно"
 
     }
 }
 
-fun fixMinutes(time: Double): String {
-return if (time/60 == 11.0) "минут" else
-    when ((time/60).toString().takeLast(1)) {
-    "1" -> "минуту"
-    "2", "3", "4" -> "минуты"
-    else -> "минут"
-}
+fun fixMinutes(time: Int): String {
+    val minutes = time / 60
+    return if ((minutes == 11) || (minutes == 12) || (minutes == 13) || (minutes == 14)) "минут" else
+        when ((time / 60).toString().takeLast(1)) {
+            "1" -> "минуту"
+            "2", "3", "4" -> "минуты"
+            else -> "минут"
+        }
 }
 
-fun fixHours(time: Double): String {
-    val hours = time/(60*60)
-    return if (hours == 11.0) "часов" else
+fun fixHours(time: Int): String {
+    val hours = time / (60 * 60)
+    return if ((hours == 11) || (hours == 12) || (hours == 13) || (hours == 14)) "часов" else
         when (hours.toString().takeLast(1)) {
             "1" -> "час"
             "2", "3", "4" -> "часа"
